8ee574f23a0c41b97f3ad77a6f9e77053f6092cb
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-02-21 21:17:23 +1100

    Add logo and update navbar

--- src/Components/Navbars.js
+++ src/Components/Navbars.js
@@ -1,14 +1,18 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Navbar, Nav, NavbarBrand } from "react-bootstrap";
+import "./Navbar.css";
+import logo from '../Images/Man-Utd-Logo.png';
 
 const Navbars = () => {
     return ( 
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="navbar" variant="dark">
             <Container>
-            <Navbar.Brand href="#home">Manchester United</Navbar.Brand>
+            <img src={logo} wdith="50px" height="50px"/>
+            <Navbar.Brand></Navbar.Brand>
+            <Navbar.Brand href="/home">Manchester United</Navbar.Brand>
             <Nav className="me-auto">
             <Nav.Link href="/home">Home</Nav.Link>
             <br></br>
-            <Nav.Link href="/features">Meet The Team</Nav.Link>
+            <Nav.Link href="/players">Meet The Team</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
             </Container>
