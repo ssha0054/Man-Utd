fd00484f0c5761c857ee307380bd46dea36860c3
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-02-22 00:06:29 +1100

    Update images

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,12 +2,15 @@ import { Col, Container, Row } from "react-bootstrap";
 import "./Home.css";
 
 import ronaldo_celebrating from '../Images/Ronaldo-celebrating.jpeg';
-// import premier_league from '../Images/PL-Trophy.jpeg';
-import premier_league from '../Images/pl_t.jpg';
+import pl from '../Images/PL-Trophy.jpeg';
+// import pl from '../Images/pl.png';
+import premier_league from '../Images/PL-Trophy.jpeg';
+import champs_league from '../Images/cl_white_bg.jpg';
 
 const Home = () => {
     return ( 
         <div className="home">
+            <br></br>
             <h2>Our Club</h2>
             <div className="our-club">
             <Container>
@@ -25,9 +28,6 @@ const Home = () => {
             </Container>
             <br></br>
             <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
             </div>
             <h2>Our History</h2>
             <div className="history">
@@ -42,6 +42,45 @@ Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Prem
                     </Col>
                     <Col className="col-3">
                     <br></br>
+                    <img src={pl} wdith="300px" height="300px"/>
+                    </Col>
+                </Row>
+            </Container>
+            <br></br>
+            </div>
+
+            <div className="our-club">
+            <Container>
+                <Row>
+                    <Col>
+                    <br></br>
+                    <img src={champs_league} wdith="300px" height="300px"/>
+                    
+                    </Col>
+                    <Col>
+                    <br></br>
+                    <br></br>
+                        <h2>Champions League</h2>
+                        Manchester United have won a record 13 titles in the Premier League since the top flight of English football was given that new name in 1992.
+Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Premier League season, in 1992/93. The Reds went on to win titles in 1994, 1996, 1997, 1999, 2000, 2001, 2003, 2007, 2008, 2009, 2011 and 2013 – more than all other clubs put together to date
+                    </Col>
+                </Row>
+            </Container>
+            <br></br>
+            </div>
+            
+            <div className="history">
+            <Container>
+                <Row>
+                    <Col className="col-2">
+                    <br></br>
+                    <br></br>
+                        <h2>European Trophies</h2>
+                        Manchester United have won a record 13 titles in the Premier League since the top flight of English football was given that new name in 1992.
+Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Premier League season, in 1992/93. The Reds went on to win titles in 1994, 1996, 1997, 1999, 2000, 2001, 2003, 2007, 2008, 2009, 2011 and 2013 – more than all other clubs put together to date
+                    </Col>
+                    <Col className="col-3">
+                    <br></br>
                     <br></br>
                     <img src={premier_league} wdith="300px" height="300px"/>
                     </Col>
@@ -49,7 +88,7 @@ Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Prem
             </Container>
             <br></br>
             </div>
-
+        
             <div className="our-club">
             <Container>
                 <Row>
@@ -62,14 +101,15 @@ Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Prem
                     <Col>
                     <br></br>
                     <br></br>
-                        <h2>Champions League</h2>
-                        add text
+                        <h2>Domestic Trophies</h2>
+                        Manchester United have won a record 13 titles in the Premier League since the top flight of English football was given that new name in 1992.
+Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Premier League season, in 1992/93. The Reds went on to win titles in 1994, 1996, 1997, 1999, 2000, 2001, 2003, 2007, 2008, 2009, 2011 and 2013 – more than all other clubs put together to date
                     </Col>
                 </Row>
             </Container>
             <br></br>
             </div>
-            
+
         </div>
      );
 }
