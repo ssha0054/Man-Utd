6c83b737c138eff7ce7820d39861746edbe02873
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-02-22 22:01:05 +1100

    Add midfielders

--- src/Components/Players.js
+++ src/Components/Players.js
@@ -13,9 +13,10 @@ const Players = () => {
             <h1>Meet The Team</h1>
             <Container>
                 <h3 className="team-grid__heading">Goalkeepers</h3>
-                <Row>
+                <Row className="ya">
                     <Col>
                         <div>
+                            <br></br>
                             <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p51940.png" style={{width: '100px'}, {height: '280px'} } />
                             <div>
                                 <h4>David De Gea</h4>
@@ -25,6 +26,7 @@ const Players = () => {
                     </Col>
                     <Col>
                         <div>
+                            <br></br>
                             <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p172649.png" style={{width: '100px'}, {height: '280px'} } />
                             <div>
                                 <h4>Dean Henderson</h4>
@@ -34,6 +36,7 @@ const Players = () => {
                     </Col>
                     <Col>
                         <div>
+                        <br></br>
                             <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p21205.png" style={{width: '100px'}, {height: '280px'} } />
                             <div>
                                 <h4>Tom Heaton</h4>
@@ -43,6 +46,7 @@ const Players = () => {
                     </Col>
                     <Col>
                         <div>
+                        <br></br>
                             <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p6744.png" style={{width: '100px'}, {height: '280px'} } />
                             <div>
                                 <h4>Lee Grant</h4>
@@ -52,6 +56,268 @@ const Players = () => {
                     </Col>
                 </Row>
             </Container>
+
+            <Container>
+                <h3 className="team-grid__heading">Defenders</h3>
+                <Row className="ya">
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p90152.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Raphael Varance</h4>
+                                <h5>18</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p95658.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Harry Maguire</h4>
+                                <h5>5</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p106760.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Luke Shaw</h4>
+                                <h5>23</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p216051.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Diogo Dalot</h4>
+                                <h5>20</h5>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="ya">
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p184667.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Victor Lindelof</h4>
+                                <h5>2</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p152590.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Alex Telles</h4>
+                                <h5>27</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p214590.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Aaron Wan-Bissaka</h4>
+                                <h5>29</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p197365.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Eric Bailey</h4>
+                                <h5>3</h5>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="ya">
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p76359.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Phil Jones</h4>
+                                <h5>4</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://manunitedcore.com/wp-content/uploads/2020/03/Brandon-Williams.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Brandon Williams</h4>
+                                <h5>33</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://bet-bet.net/wp-content/uploads/2020/10/Axel-Tuanzebe.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Axel Tuanzebe</h4>
+                                <h5>38</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p244856.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Teden Mengi</h4>
+                                <h5>3</h5>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container>
+                <h3 className="team-grid__heading">Midfielders</h3>
+                <Row className="ya">
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p141746.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Bruno Fernandes</h4>
+                                <h5>18</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p74208.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Paul Pogba</h4>
+                                <h5>6</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p195851.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Scott McTominay</h4>
+                                <h5>39</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p101582.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Fred</h4>
+                                <h5>17</h5>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="ya">
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p109322.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Jesse Lingard</h4>
+                                <h5>14</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://www.thesportsdb.com/images/media/player/cutout/11ghko1631792134.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Donny Van De Beek</h4>
+                                <h5>34</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p62398.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Nemanja Matic</h4>
+                                <h5>31</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p43670.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Juan Mata</h4>
+                                <h5>8</h5>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="ya">
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p493250.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Amad</h4>
+                                <h5>16</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p488404.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Facundo Pellistri</h4>
+                                <h5>28</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p232928.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>James Garner</h4>
+                                <h5>37</h5>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                        <br></br>
+                            <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p465527.png" style={{width: '100px'}, {height: '280px'} } />
+                            <div>
+                                <h4>Hannibal</h4>
+                                <h5>46</h5>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+            
 
             <Container>
                 <h3 className="team-grid__heading">Midfielders</h3>
