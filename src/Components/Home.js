133043a2efaf9c6d966b13724a575dbb7718a5b9
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-02-22 18:18:13 +1100

    Add div for footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,18 +10,20 @@ import champs_league from '../Images/cl_white_bg.jpg';
 const Home = () => {
     return ( 
         <div className="home">
+            <br></br> 
+            <br></br> 
             <br></br>
-            <h2>Our Club</h2>
+            <br></br> 
+            <h2 position="right">Our Club</h2>
             <div className="our-club">
             <Container>
                 <Row>
                     <Col>
                         <br></br>
                         <img src={ronaldo_celebrating} wdith="300px" height="300px"/>
-                    
                     </Col>
                     <Col>
-                    <h2>Suiiiiii</h2>
+                    <h2>Manchester United Football Club</h2>
                     Manchester United have won a record 13 titles in the Premier League since the top flight of English football was given that new name in 1992.
 Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Premier League season, in 1992/93. The Reds went on to win titles in 1994, 1996, 1997, 1999, 2000, 2001, 2003, 2007, 2008, 2009, 2011 and 2013 – more than all other clubs put together to date
 Manchester United have won a record 13 titles in the Premier League since the top flight of English football was given that new name in 1992.
@@ -114,6 +116,9 @@ Sir Alex Ferguson was the man who masterminded the triumph in the inaugural Prem
             <br></br>
             </div>
 
+            <div className="footer" fixed="bottom">
+                <br></br>
+            </div>
         </div>
      );
 }
